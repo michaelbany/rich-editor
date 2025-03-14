4ffc2d322e6bf0627749797526022de07ecfa211
Michael Baný <mobany526@gmail.com>
2025-03-14 11:47:19 +0100

    cursor position

--- composables/keydown.ts
+++ composables/keydown.ts
@@ -1,63 +1,66 @@
 export function keydownAPI(context: EditorContext) {
-    return {
-        trigger: (e: KeyboardEvent) => {
-            switch (e.key) {
-                case "ArrowUp":
-                    context.KeyDown.arrow(e, 'up');
-                    break;
-                case "ArrowDown":
-                    context.KeyDown.arrow(e, 'down');
-                    break;
+  return {
+    trigger: (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowUp":
+          context.KeyDown.arrow(e, "up");
+          break;
+        case "ArrowDown":
+          context.KeyDown.arrow(e, "down");
+          break;
+      }
+    },
+    /**
+     * Simple implementation of arrow key navigation.
+     * #note Have to be fixed to handle more complex cases.
+     */
+    arrow: (e: KeyboardEvent, direction: "up" | "down") => {
+      const cursor = context.state.cursor.get();
+      const currentBlock = context.Block.find(cursor?.block);
+      if (!cursor || !currentBlock) return;
+
+      const block = direction === "up" ? currentBlock.previous() : currentBlock.next();
+
+      if (block) {
+
+        if (direction === "up" && cursor.absolute.start === 0) {
+          e.preventDefault();
+          context.Cursor.move(block, cursor.absolute.start);
+          return;
+        }
+        if (direction === "down" && cursor.absolute.end === 0) {
+          e.preventDefault();
+          context.Cursor.move(block, cursor.absolute.start);
+          return;
+        }
+
+        requestAnimationFrame(() => {
+          requestAnimationFrame(() => {
+            if (direction === "up" && context.state.cursor.get()?.absolute.start === 0) {
+              e.preventDefault();
+              context.Cursor.move(block, cursor.absolute.start);
+              return;
             }
-        },
-        /**
-         * #todo: If block has more rows should move to the next row
-         * instead of the next block.
-         */
-        arrow: (e: KeyboardEvent, direction: 'up'|'down') => {
-            const oldCursor = context.state.cursor.get();
-            if (!oldCursor) return;
-
-            const oldOffset = oldCursor.absolute.start;
-
-            requestAnimationFrame(() => {
-                const newCursor = context.state.cursor.get();
-                if (!newCursor) return;
-
-                if (newCursor.absolute.start === oldOffset) {
-                    console.log('Move to the previous block WRONG');
-                } else {
-                    console.log('Stay WRONG');
-                }
-            });
-
-
-            // const currentBlock = context.Block.find(context.state.cursor.get()?.block);
-            // let block;
-            // const startIndex = context.state.cursor.get()?.absolute.start;
-
-            // if (direction === 'up') {
-            //     block = currentBlock?.previous();
-            // } else {
-            //     block = currentBlock?.next();
-            // }
-
-            // if (block) {
-            //     e.preventDefault();
-            //     context.Cursor.move(block, startIndex ?? 0);
-            // }
-        },
-        validate: (e: KeyboardEvent): boolean => {
-            if (!e.target) return false;
-
-            if (!(e.target instanceof HTMLElement) || !context.Block.find(e.target.id)) {
-                console.warn("Input event target is not a valid block element.");
-                return false;
+            if (direction === "down" && context.state.cursor.get()?.absolute.end === 0) {
+              e.preventDefault();
+              context.Cursor.move(block, cursor.absolute.start);
+              return;
             }
+          });
+        });
+      }
+    },
+    validate: (e: KeyboardEvent): boolean => {
+      if (!e.target) return false;
 
-            return true;
-        },
-    }
+      if (!(e.target instanceof HTMLElement) || !context.Block.find(e.target.id)) {
+        console.warn("Input event target is not a valid block element.");
+        return false;
+      }
+
+      return true;
+    },
+  };
 }
 
-export type KeydownAPI = ReturnType<typeof keydownAPI>
+export type KeydownAPI = ReturnType<typeof keydownAPI>;
--- pages/index.vue
+++ pages/index.vue
@@ -38,6 +38,14 @@
       ],
       props: {},
     },
+    {
+      type: "paragraph",
+      nodes: [
+        {
+          text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Quisque tincidunt scelerisque libero. Etiam posuere lacus quis dolor. Etiam commodo dui eget wisi. Integer pellentesque quam vel velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Mauris elementum mauris vitae tortor. Vivamus luctus egestas leo. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Donec iaculis gravida nulla. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. In rutrum. Sed ac dolor sit amet purus malesuada congue. Aliquam erat volutpat. Praesent in mauris eu tortor porttitor accumsan."
+        }
+      ]
+    }
   ];
 
   // const editor = useEditor(content)
